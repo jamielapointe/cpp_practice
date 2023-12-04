40df23bf295967d52cc716d77c15871ce684b5ce
Jamie LaPointe <jamie.lapointe@gmail.com>
2023-12-03 17:22:13 -0700

    CP-5 Cleanup Endianess wrapper & test

--- include/cpp_practice/endianess.h
+++ include/cpp_practice/endianess.h
@@ -3,7 +3,7 @@
 namespace cpp_practice::endianess {
 
 ///\brief endianess - byte ordering
-enum class endianess { little_endian, big_endian, unknown };
+enum class endianess { little_endian, big_endian, mixed_endian };
 
 ///\brief return \b true if the system is little endian
 constexpr bool is_little_endian() {
@@ -15,15 +15,29 @@ constexpr bool is_big_endian() {
   return std::endian::native == std::endian::big;
 }
 
+///\brief return \b true if the system is mixed endian
+///\note only the PDP-11 architecture is mixed endian to the best of my
+/// knowledge
+constexpr bool is_mixed_endian() {
+  return !is_little_endian() && !is_big_endian();
+}
+
 ///\brief return the endianess of the system
 constexpr endianess get_endianess() {
-  if (is_little_endian()) {
+  if constexpr (is_little_endian()) {
     return endianess::little_endian;
-  } else if (is_big_endian()) {
-    return endianess::big_endian;
-  } else {
-    return endianess::unknown;
   }
+
+  if constexpr (is_big_endian()) {
+    return endianess::big_endian;
+  }
+
+  if constexpr (is_mixed_endian()) {
+    return endianess::mixed_endian;
+  }
+
+  // Note that it must be one of the options, so this is unreachable.
+  // If for some reason all are false, then we get a compiler error.
 }
 
 }  // namespace cpp_practice::endianess
